38cc294453ee2cdb924d686f178ac96fb8aa1bb1
sergiorios777 <sergio.rios777@gmail.com>
2025-05-27 15:58:06 -0500

    Agregó la ruta de grupo products para la resolución de los métodos del controlador

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,3 +9,13 @@ service('auth')->routes($routes);
 
 $routes->get('/', 'Home::index');
 $routes->get('app', 'DashboardController::index');
+
+$routes->group('products', ['filter' => 'session'], function ($routes) { // 'session' es el filtro de auth de Shield
+    $routes->get('/', 'ProductController::index');
+    $routes->get('new', 'ProductController::new');
+    $routes->post('create', 'ProductController::create');
+    $routes->get('edit/(:num)', 'ProductController::edit/$1');
+    $routes->post('update/(:num)', 'ProductController::update/$1'); // CodeIgniter maneja _method="PUT" si envías ese campo, o puedes usar $routes->put()
+    $routes->get('delete/(:num)', 'ProductController::delete/$1'); // Para MVP. Mejor usar POST o DELETE con un form.
+    // $routes->post('delete/(:num)', 'ProductController::delete/$1'); // Si usas un form para el delete
+});
