3533106ebacfb4f75b7a88735111b50cc9802ba9
sergiorios777 <sergio.rios777@gmail.com>
2025-05-29 10:24:33 -0500

    Se agregó Supplier entity

--- app/Database/Migrations/2025-05-29-152023_CreateSuppliersTable.php
+++ app/Database/Migrations/2025-05-29-152023_CreateSuppliersTable.php
@@ -8,11 +8,43 @@ class CreateSuppliersTable extends Migration
 {
     public function up()
     {
-        //
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 5,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'name' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'ruc' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '20', // Ajusta la longitud según tus necesidades para RUC
+                'null'       => true,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'deleted_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('suppliers');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('suppliers');
     }
 }
