5e9bc4a03a68369f470b89c57ab6020e9ff8c5ce
sergiorios777 <sergio.rios777@gmail.com>
2025-05-29 10:00:38 -0500

    Se agregaron las rutas y alias en Config/Routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -26,3 +26,13 @@ $routes->group('purchases', ['filter' => 'session'], function ($routes) {
     $routes->post('create', 'PurchaseController::create');
     $routes->get('show/(:num)', 'PurchaseController::show/$1');
 });
+
+$routes->group('customers', ['filter' => 'session'], function ($routes) { // 'session' es el filtro de auth de Shield
+    $routes->get('/', 'CustomerController::index', ['as' => 'customer_list']);
+    $routes->get('new', 'CustomerController::new', ['as' => 'customer_new']);
+    $routes->post('create', 'CustomerController::create', ['as' => 'customer_create']);
+    $routes->get('edit/(:num)', 'CustomerController::edit/$1', ['as' => 'customer_edit']);
+    $routes->post('update/(:num)', 'CustomerController::update/$1', ['as' => 'customer_update']);
+    $routes->get('delete/(:num)', 'CustomerController::delete/$1', ['as' => 'customer_delete']); // Para MVP GET con confirmación JS
+    // Considera $routes->post('delete/(:num)', 'CustomerController::delete/$1'); para producción
+});
